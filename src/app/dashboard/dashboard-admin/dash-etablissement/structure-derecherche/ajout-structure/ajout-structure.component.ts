cfdb9f913fcff25253bbbae1739f4a8966dc9daa
Chaima-DB <chimox751@gmail.com>
2020-06-12 16:49:29 +0100

    espace Etablissements et structures

---
+++ src/app/dashboard/dashboard-admin/dash-etablissement/structure-derecherche/ajout-structure/ajout-structure.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {StructureDeRechercheService} from '../../../../../controller/service/structure-de-recherche.service';
+import {EtablissementService} from '../../../../../controller/service/etablissement.service';
+import {StructureDeRecherche} from '../../../../../controller/model/structure-de-recherche.model';
+import {Etablissement} from '../../../../../controller/model/etablissement.model';
+
+@Component({
+  selector: 'app-ajout-structure',
+  templateUrl: './ajout-structure.component.html',
+  styleUrls: ['./ajout-structure.component.css']
+})
+export class AjoutStructureComponent implements OnInit {
+private _i: number;
+
+  constructor(
+    private structureDeRechercheService: StructureDeRechercheService,
+    private etablissementService: EtablissementService
+  ) { this._i = 1; }
+
+  ngOnInit(): void {
+    this.etablissementService.findAll();
+  }
+
+  get i(): number {
+    return this._i;
+  }
+
+
+  set i(value: number) {
+    this._i = value;
+  }
+
+  get structure(): StructureDeRecherche {
+    return this.structureDeRechercheService.structure;
+  }
+  get etablissement(): Etablissement {
+    return this.etablissementService.etablissement;
+  }
+  get etablissements(): Array<Etablissement> {
+    return this.etablissementService.etablissements;
+  }
+
+  get ok(): string {
+    return this.structureDeRechercheService.ok;
+  }
+  get no(): string {
+    return this.structureDeRechercheService.no;
+  }
+
+  public save() {
+    this.structureDeRechercheService.save();
+  }
+}
---
+++ src/app/dashboard/dashboard-admin/dash-etablissement/structure-derecherche/list-structure/list-structure.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {StructureDeRecherche} from '../../../../../controller/model/structure-de-recherche.model';
+import {Etablissement} from '../../../../../controller/model/etablissement.model';
+import {EtablissementService} from '../../../../../controller/service/etablissement.service';
+import {StructureDeRechercheService} from '../../../../../controller/service/structure-de-recherche.service';
+
+@Component({
+  selector: 'app-list-structure',
+  templateUrl: './list-structure.component.html',
+  styleUrls: ['./list-structure.component.css']
+})
+export class ListStructureComponent implements OnInit {
+
+
+  constructor(
+    private structureDeRechercheService: StructureDeRechercheService,
+    private etablissementService: EtablissementService
+  ) { }
+
+  ngOnInit(): void {
+    this.structureDeRechercheService.findAll();
+    this.etablissementService.findAll();
+  }
+  get structure(): StructureDeRecherche {
+    return this.structureDeRechercheService.structure;
+  }
+  get structures(): Array<StructureDeRecherche> {
+    return this.structureDeRechercheService.structures;
+  }
+  get etablissement(): Etablissement {
+    return this.etablissementService.etablissement;
+  }
+  get etablissements(): Array<Etablissement> {
+    return this.etablissementService.etablissements;
+  }
+
+  get ok(): string {
+    return this.structureDeRechercheService.ok;
+  }
+  get no(): string {
+    return this.structureDeRechercheService.no;
+  }
+
+  public saveStructure() {
+    this.structureDeRechercheService.save();
+  }
+  public deleteByReference(structure: StructureDeRecherche) {
+    this.structureDeRechercheService.deleteByReference(structure);
+  }
+}
