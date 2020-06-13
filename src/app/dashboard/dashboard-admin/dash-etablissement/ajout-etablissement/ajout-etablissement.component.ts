c2eae6ad27c54b68dc482d445f65c2eddca0d632
Yahya-Md <Yahya-Md@users.noreply.github.com>
2020-06-13 02:20:47 +0100

    fix-4

--- src/app/dashboard/dashboard-admin/dash-etablissement/ajout-etablissement/ajout-etablissement.component.ts
+++ src/app/dashboard/dashboard-admin/dash-etablissement/ajout-etablissement/ajout-etablissement.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {EtablissementService} from '../../../../controller/service/etablissement.service';
 import {Etablissement} from '../../../../controller/model/etablissement.model';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-ajout-etablissement',
@@ -11,20 +12,25 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class AjoutEtablissementComponent implements OnInit {
   public etablissementForm: FormGroup;
   private _isReset: boolean;
-  constructor(private etablissementService: EtablissementService) {this._isReset = false;
+
+  constructor(private etablissementService: EtablissementService, private _snackBar: MatSnackBar) {this._isReset = false;
   }
+
   requiredFormControlreference = new FormControl('',[
     Validators.required,
   ]);
   requiredFormControlnom = new FormControl('',[
     Validators.required,
   ]);
+
   ngOnInit(): void {
     this.etablissementForm = new FormGroup({
       reference: new FormControl('', [Validators.required, Validators.maxLength(60)]),
       nom: new FormControl('', [Validators.required, Validators.maxLength(100)])
     });
   }
+
+  // ----- Service -------
   get etablissement(): Etablissement {
     return this.etablissementService.etablissement;
   }
@@ -45,10 +51,6 @@ export class AjoutEtablissementComponent implements OnInit {
   get result(): number {
     return this.etablissementService.result;
   }
-  public save() {
-    this.etablissementService.save();
-    this.etablissementForm.reset();
-  }
   public deleteByReference(etablissement: Etablissement) {
     this.etablissementService.deleteByReference(etablissement);
   }
@@ -56,4 +58,20 @@ export class AjoutEtablissementComponent implements OnInit {
     this._isReset = true;
     this.etablissementService.etablissement = null;
   }
+  public save() {
+    this.etablissementService.save();
+    this.etablissementForm.reset();
+
+  }
+// ----- snack bar ---------------
+  durationInSeconds = 5;
+  openSnackBar() {
+    if (this.ok != null){
+      this._snackBar.open('ok');
+    }else if (this.no != null) {
+      this._snackBar.open('no');
+    }
+
+  }
 }
+
